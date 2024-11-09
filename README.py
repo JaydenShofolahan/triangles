b37a5c69b89b779ac746f2df21922df571897148
JaydenShofolahan <jaydenshofolahan@gmail.com>
2024-11-09 13:13:42 -0500

    Update README.py

--- README.py
+++ README.py
@@ -1,43 +1,64 @@
 #Name: Jayden 
 #Date: 11/09/2024 
 #Description: This program will draw two different unique triangles 
-
+#CSci 127 Teaching Staff
+#February 2018
+#A template for a program that draws nested triangles 
+#Modified by:  ADD YOUR NAME HERE
 
 import turtle
+def setUp(t, dist, col):
+    """
+    Takes three parameters, a turtle, t, the distance, dist, 
+    to move the turtle and a color, col, to set the turtle's color.
+    """
+    t.penup()
+    t.forward(dist)
+    t.pendown()
+    t.color(col)
 
-def start(tr, forward_user, color_user): 
-  tr = turtle.Turtle() 
-  
-  tr.penup() 
-  tr.forward(forward_user) 
-  tr.pendown() 
-  tr.color(color_user) 
-                     
-def triangle(tr, len): 
-  if len > 10:
-    for i in range(3):
-      tr.forward(len)
-      tr.left(120)
-    return triangle(tr, len/2) 
 
-def nestedTriange(tr, len):
-  if len > 10:
-    for i in range(3): 
-      tr.forward(len)
-      tr.left(120)
-      return nestedTriangle(tr, len/2) 
+def triangle(t, length):
+    """
+    Takes two parameters: a turtle and a length.
+    The function does the following: if the length is greater than 10,
+    it repeats 3 times:  moves forward that length, turns 120 degrees, 
+    and calls triangle(t, length/2).
+    """
+    if len > 10:
+      for i in range(3):
+        t.forward(len)
+        t.left(120)
+      return triangle(tr, len/2)    
+
+
+def nestedTriangle(t, length):
+    """
+    Takes two parameters: a turtle and a length.
+    The function does the following: if the length is greater than 10,
+    it repeats 3 times:  moves forward that length, turns 120 degrees, 
+    and calls triangle(t, length/2).
+    """
+
+     if len > 10:
+       for i in range(3): 
+         tr.forward(len)
+         tr.left(120)
+         return nestedTriangle(tr, len/2)       
 
 
 def main():
-  n = input("Enter a length") 
-  tom = turtle.Turtle()
-  start(tom, -100, "steelblue") 
-  nestedTriangle(tom, n) 
+    n = int(input('Enter length: '))
 
-  tess = turtle.Turtle() 
-  start(tess, 100, "steelblue") 
-  nestedTriangle(tess, n) 
+    tom = turtle.Turtle()
+    setUp(tom, -100, "darkgreen")
+    triangle(tom, n)
+
+    tess = turtle.Turtle()
+    setUp(tess, 100, "steelblue")
+    nestedTriangle(tess, n)
 
 if __name__ == "__main__":
-  main() 
+     main()
+
 
